1dbf298699ee13250a76214afcbe35eb10f7b935
Michael <mreyna12@live.com>
2022-11-01 13:08:24 -0500

    make responsive up to roughly 1900px

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,44 +1,62 @@
+import { useState } from "react";
+
 function Nav() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenuToggle() {
+    setShowMenu((prevState) => !prevState);
+  }
+
   return (
     <nav className="nav-padding">
-      <div className="container space-between-center">
+      <div className="container nav-space-between-center">
         <div>
           <a href="#">
             <img src={"images/logo.svg"} alt="Easybank logo" />
           </a>
         </div>
-        <div className="nav-items-container navbar-menu">
-          <ul className="nav-items">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Careers</a>
-            </li>
-          </ul>
-        </div>
+
+        <ul data-visible={showMenu} className="nav-items mobile-menu">
+          <li>
+            <a onClick={handleMenuToggle} href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a onClick={handleMenuToggle} href="#">
+              About
+            </a>
+          </li>
+          <li>
+            <a onClick={handleMenuToggle} href="#">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a onClick={handleMenuToggle} href="#">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a onClick={handleMenuToggle} href="#">
+              Careers
+            </a>
+          </li>
+        </ul>
+
         <div className="navbar-menu">
           <button>Request Invite</button>
         </div>
         <a
-          href="#"
+          onClick={handleMenuToggle}
           className="navbar-burger"
           role={"button"}
           aria-label="menu"
-          aria-expanded="false"
+          aria-expanded={showMenu}
         >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </a>
       </div>
     </nav>
